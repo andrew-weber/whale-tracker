8cb9f32759f20211185e7a991f397388c6f1b47b
Andrew Weber <andrew.weber@truepill.com>
2022-04-01 21:20:47 -0400

    Refactor

---
+++ reporter/pages/api/graphql.ts
@@ -0,0 +1,41 @@
+import { gql, ApolloServer } from 'apollo-server-micro';
+
+const typeDefs = gql`
+  type User {
+    id: ID
+  }
+
+  type Query {
+    getUser: User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUser: () => {
+      return {
+        id: 'Foo',
+      };
+    },
+  },
+};
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = apolloServer.start();
+
+export default async function handler(req: any, res: any) {
+  await startServer;
+  await apolloServer.createHandler({
+    path: "/api/graphql",
+  })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
