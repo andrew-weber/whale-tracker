36fdb96367a5868b08436e1c976ef5923a030ba6
Andrew Weber <andrew.weber@truepill.com>
2022-05-25 12:10:00 -0400

    log port

--- reporter/apollo-client.ts
+++ reporter/apollo-client.ts
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+console.log('here', process.env.PORT)
 const client = new ApolloClient({
     uri: `http://localhost:${process.env.PORT || '1337'}/api/graphql`,
     cache: new InMemoryCache(),
