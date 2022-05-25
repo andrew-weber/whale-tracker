d3d0715f2a0fbeec1227423cd9db5ccd68947f2b
Andrew Weber <andrew.weber@truepill.com>
2022-05-25 12:27:27 -0400

    remove base_url

--- reporter/apollo-client.ts
+++ reporter/apollo-client.ts
@@ -1,8 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-console.log('here', process.env.PORT)
 const client = new ApolloClient({
-    uri: `http://localhost:${process.env.PORT || '1337'}/api/graphql`,
+    uri: `/api/graphql`,
     cache: new InMemoryCache(),
 });
 
