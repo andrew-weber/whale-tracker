afa56ab5525bc076837df7ca532688d181e80be8
Andrew Weber <andrew.weber@truepill.com>
2022-11-15 12:07:01 -0500

    fix: tweet validator properly grabs the correct option type

--- reader/helpers/tweet-validator.js
+++ reader/helpers/tweet-validator.js
@@ -9,9 +9,11 @@ export const parseTweet = (text) => {
     return null;
   }
 
-  const ticker = RegExp(/^[$]\S{1,5}/).exec(text)[0];
+  const ticker = RegExp(/^[$]\S{1,6}/).exec(text)[0];
   const expiry = RegExp(/\d{4}-\d{2}-\d{2}/).exec(text)[0];
-  const option_type = RegExp(/[CP]/).exec(text)[0];
+  const option_type = RegExp(/\s[CP]\s/)
+    .exec(text)[0]
+    .trim();
   const [strike_price, underlying, bid, ask] = [
     ...text.matchAll(/[$]\d+.?\d*/g),
   ].map((obj) => obj[0]);
